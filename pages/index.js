5e778216f620416f3dcff8e8802eec8832f5a966
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-02 12:38:06 +0800

    initial layout configs, a few pages

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Header from "@components/Header";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <main>
+      <Header title="Event guide" />
+      <p className="description">Skeleton site for event guide</p>
+      <p>By Lauri Jalonen</p>
+    </main>
+  );
 }
