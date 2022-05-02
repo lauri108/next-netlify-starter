26135c330b5e7159ccaa4d125dac50da8873c03e
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-02 16:32:36 +0800

    added MUI and some test components

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Container } from "@mui/material";
 
 export default function Layout({ children }) {
   return (
     <>
-      <div className="container">
+      <Container maxWidth="lg">
         <Navbar />
         <main>{children}</main>
         <Footer />
-      </div>
+      </Container>
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-import "@styles/globals.css";
-import Layout from "../components/Layout";
-import Head from "next/head";
-import Footer from "@components/Footer";
-import React from "react";
+import * as React from "react";
 import PropTypes from "prop-types";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
 import theme from "../styles/theme";
+import createEmotionCache from "../components/createEmotionCache";
+import Layout from "@components/Layout";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 function Application(props) {
-  const { Component, pageProps } = props;
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -19,18 +22,26 @@ function Application(props) {
     }
   }, []);
   return (
-    <Layout>
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Layout>
+    <CacheProvider value={emotionCache}>
+      <Layout>
+        <Head>
+          <title>Next.js Starter!</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Layout>
+    </CacheProvider>
   );
 }
 
 export default Application;
+
+Application.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
