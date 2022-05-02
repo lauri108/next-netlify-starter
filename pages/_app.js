8578851a347d083369ac7471dc267219669368b3
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-02 13:36:38 +0800

    added material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import Layout from "../components/Layout";
+import Head from "next/head";
+import Footer from "@components/Footer";
+import React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../styles/theme";
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Application(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Layout>
+  );
 }
 
 export default Application
