f6fcdb145a755da6b17ab08fbede1c02c8485416
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-03 18:38:31 +0800

    event fetch based on id

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,5 +1,13 @@
 import Header from "@components/Header";
+import {getEventId} from '../../dummy-data'
 function EventDetailsPage() {
+  const router = useRouter()
+  const eventId = router.query.eventId
+  const event = getEventId(eventId)
+  if(!event) {
+    return <p>No event found!</p>
+  }
+
   return (
     <main>
       <Header title="Event detail" />
